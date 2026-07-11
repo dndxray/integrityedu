8635154578c739d9af29a32326adc66209785872
Dinda Isyariani <isyarianid2@g,ail.com>
2026-07-12 05:16:29 +0700

    restore

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -50,3 +50,18 @@ def create_access_token(data: dict):
         SECRET_KEY,
         algorithm=ALGORITHM
     )
+
+from jose import JWTError, jwt
+
+
+def decode_access_token(token: str):
+    try:
+        payload = jwt.decode(
+            token,
+            SECRET_KEY,
+            algorithms=[ALGORITHM]
+        )
+        return payload
+
+    except JWTError:
+        return None
